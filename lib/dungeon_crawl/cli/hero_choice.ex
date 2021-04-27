654f0c098d24fa069fa0c37b0935ae2c03673a28
Diego Alvarez <andresdaap2015@gmail.com>
2021-04-27 11:38:17 -0400

    Refactoring

--- lib/dungeon_crawl/cli/hero_choice.ex
+++ lib/dungeon_crawl/cli/hero_choice.ex
@@ -1,5 +1,6 @@
 defmodule DungeonCrawl.CLI.HeroChoice do
   alias Mix.Shell.IO, as: Shell
+  import DungeonCrawl.CLI.BaseCommands
 
   def start do
     Shell.cmd("clear")
@@ -9,29 +10,12 @@ defmodule DungeonCrawl.CLI.HeroChoice do
     find_hero_by_index = &Enum.at(heroes, &1)
     
     heroes
-    |> Enum.map(&(&1.name))
     |> display_options
-    |> generate_questions
+    |> generate_question
     |> Shell.prompt
     |> parse_answer
     |> find_hero_by_index.()
     |> confirm_hero
-  end
-  def display_options(options) do
-    options
-    |> Enum.with_index(1)
-    |> Enum.each(fn {option, index} ->
-      Shell.info("#{index} - #{option}")
-    end)
-    options
-  end
-  defp generate_questions(options) do
-    options = Enum.join(1..Enum.count(options), ",")
-    "Which one?... [#{options}]\n"
-  end
-  defp parse_answer(answer) do
-    {option, _} = Integer.parse(answer)
-    option - 1
   end
   defp confirm_hero(chosen_hero) do
     Shell.cmd("clear")
