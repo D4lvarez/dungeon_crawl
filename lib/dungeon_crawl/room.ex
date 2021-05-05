2af56d1b9144df2cf48e7877224407b8948f8bfb
Diego Alvarez <andresdaap2015@gmail.com>
2021-05-04 23:21:47 -0400

    Finish Game

--- lib/dungeon_crawl/room.ex
+++ lib/dungeon_crawl/room.ex
@@ -1,14 +1,21 @@
 defmodule DungeonCrawl.Room do
   alias DungeonCrawl.Room
+  alias DungeonCrawl.Room.Triggers
 
   import DungeonCrawl.Room.Action
 
-  defstruct description:  nil, actions: []
+  defstruct description:  nil, actions: [], trigger: nil
 
   def all, do: [
-  %Room{
-    description: "You found a quiet place. Looks safe for a little nap",
-    actions: [forward(), rest()],
-  },
+    %Room{
+      description: "You found a quiet place. Looks safe for a little nap",
+      actions: [forward()],
+      trigger: Triggers.Exit
+    },
+    %Room{
+      description: "You can see a enemy blocking your path",
+      actions: [forward()],
+      trigger: Triggers.Enemy
+    },
   ]
 end
